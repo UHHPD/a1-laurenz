b6a31d85db8cd359939e5e7415244e39f76d8f71
Laurenz Kahl <102945441+LaurenzKahl@users.noreply.github.com>
2022-04-28 09:00:44 +0000

    Stichprobe

--- stichprobe2.cc
+++ stichprobe2.cc
@@ -4,30 +4,27 @@
 using namespace std;
 
 double a , b, c, d ,e ,f;
+ifstream fin ("datensumme.txt")
 
 int Mittelwert(){
-  ifstream fin ("datensumme.txt");
-  for (int j = 0; j<26; j++){
-    for (int i = 0; i<9 ; i++){
-      fin >> a;
-      b +=a;
-    }
-    c = b/9;
-    cout << c << endl;
-    b = 0;
+  for (int i = 0; i<9 ; i++){
+    fin >> a;
+    b +=a;
   }
-  fin.close();  
+  c = b/9;
+  cout << c << endl;
+  fout << c << endl;
+  b = 0;
 }
 
 int Varianz(){
-  ifstream fin ("datensumme.txt");
-  for (int i = 0; i<234 ;i++){
-    fin >> d;
-    e += (d-c) * (d-c);
+    for (int i = 0; i<9 ;i++){
+      fin2 >> d;
+      e += (d-c) * (d-c);
   }
-  f = e/234; 
+  f = e/9; 
   cout << f << endl;
-  fin.close();
+  e = 0; 
 }
 
 int Standartabweichung(){
@@ -35,7 +32,19 @@ int Standartabweichung(){
 }
 
 int main(){
-Mittelwert();
-Varianz();
-Standartabweichung();
+  ifstream fin ("datensumme.txt");
+  ifstream fin2 ("datensumme.txt");
+  ofstream fout ("mittelwerte.txt"); 
+  ofstream fout2 ("varienzen.txt");
+  for (int j = 0; j<26; j++){
+    Mittelwert();
+      fout << c << endl;
+    Varianz();
+      fout2 << c << endl;
+    Standartabweichung();
+  }
+  fin.close();
+  fin2.close(); 
+  fout.close(); 
+  fout2.close();
 }
